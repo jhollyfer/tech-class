ee1ab63edd77d93c10332ec8434569c34cc73532
indiodev <jhollyfer.fr@gmail.com>
2026-03-24 23:52:38 -0500

    nova versao coolify

--- backend/start/env.ts
+++ backend/start/env.ts
@@ -11,21 +11,28 @@
 
 import { Env } from '@adonisjs/core/env'
 
-export default await Env.create(new URL('../', import.meta.url), {
-  // Node
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string(),
+const isNodeAce = process.argv.some((arg) => arg.endsWith('/ace') || arg === 'ace')
 
-  // App
-  APP_KEY: Env.schema.secret(),
-  APP_URL: Env.schema.string({ format: 'url', tld: false }),
+export default await Env.create(
+  new URL('../', import.meta.url),
+  isNodeAce
+    ? {}
+    : {
+        // Node
+        NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+        PORT: Env.schema.number(),
+        HOST: Env.schema.string({ format: 'host' }),
+        LOG_LEVEL: Env.schema.string(),
 
-  // Database
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string(),
-  DB_DATABASE: Env.schema.string(),
-})
+        // App
+        APP_KEY: Env.schema.secret(),
+        APP_URL: Env.schema.string({ format: 'url', tld: false }),
+
+        // Database
+        DB_HOST: Env.schema.string({ format: 'host' }),
+        DB_PORT: Env.schema.number(),
+        DB_USER: Env.schema.string(),
+        DB_PASSWORD: Env.schema.string(),
+        DB_DATABASE: Env.schema.string(),
+      }
+)
